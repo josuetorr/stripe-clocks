6ddf90a2664c80ed6198488d384262022c29d1a6
Josue Torres <josue.dtorres@gmail.com>
2022-05-05 01:21:23 -0400

    added function for handling request

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,11 +3,14 @@ import { InvalidArgError } from "../cmd/index.js";
 import { HandlerProps, Card } from "../interfaces/index.js";
 
 export abstract class StripeHandler {
-  protected _stripe: Stripe;
   private _paymentMethod?: string;
   private _card?: Card;
   private _startAt: number;
-  private _endAt: number;
+  private _name: string;
+  private _email: string;
+
+  protected _stripe: Stripe;
+  protected _endAt: number;
 
   constructor(props: HandlerProps) {
     if (!props.apiKey)
@@ -26,12 +29,23 @@ export abstract class StripeHandler {
       };
     }
 
-    const today = new Date()
-    this._startAt = props.startAt ?? today.getTime()
+    const today = new Date();
+    this._startAt = props.startAt
+      ? new Date(props.startAt).getTime()
+      : today.getTime();
     this._endAt = props.endAt
+      ? new Date(props.endAt).getTime()
+      : today.setMonth(today.getMonth() + 1);
+    this._name = props.name ?? "test";
+    this._email = props.email ?? "jim@bob.com";
   }
 
-  abstract handleRequest(): Promise<void>;
+  private getClock() {
+    return this._stripe.testHelpers.testClocks.create({
+      frozen_time: this._startAt,
+      name: this._name,
+    });
+  }
 
   getPaymentMethod(): Promise<Stripe.PaymentMethod> {
     if (this._paymentMethod)
@@ -44,5 +58,27 @@ export abstract class StripeHandler {
       card: this._card,
     });
   }
-}
 
+  async getCustomer() {
+    return this._stripe.customers.create({
+      email: this._email,
+      test_clock: (await this.getClock()).id,
+    });
+  }
+
+  async attachPaymentToCustomer(
+    customer: Stripe.Customer,
+    paymentMethod: Stripe.PaymentMethod,
+    makeItDefault = true
+  ) {
+    this._stripe.paymentMethods.attach(paymentMethod.id, {
+      customer: customer.id,
+    });
+    if (makeItDefault)
+      this._stripe.customers.update(customer.id, {
+        invoice_settings: { default_payment_method: paymentMethod.id },
+      });
+  }
+
+  abstract handleRequest(): Promise<void>;
+}
